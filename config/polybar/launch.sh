cf54f9bb4969d539e3a3dd339aa07bd6369d944c
Akhil Guntur <aguntur12@gmail.com>
2021-11-24 14:23:16 -0800

    add files
     - alacritty, i3, polybar, rofi, vim

---
+++ config/polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar -c $HOME/.config/polybar/config example
+
+echo "Polybar launched..."
