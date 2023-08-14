2bef7f86f91413f12252399159f6bebe0e4252b3
Akhil Guntur <aguntur12@gmail.com>
2023-08-14 15:28:49 -0700

    work on rofi calendar, misc. - switched from nautilus to thunar - switched from zsh to fish

--- .config/rofi/calendar.sh
+++ .config/rofi/calendar.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-{ ncal -bh; echo -en "\x0f"; ncal -b 2 2022;} | rofi -dmenu -sep '\x0f' -eh 9 -theme ~/.config/rofi/calendar-uncreative.rasi
+print=""
+
+for i in {1..31}
+do
+	print="${print}${i}\n"
+done
+
+echo -e "$print" | rofi -dmenu -theme ~/.config/rofi/calendar-uncreative.rasi
